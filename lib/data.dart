be59d7dc4d49168f23cd49e4ef955fddf7871f58
mamad2050 <mamadzn2050@gmail.com>
2023-09-22 00:44:48 +0330

    init hivedb

--- lib/data.dart
+++ lib/data.dart
@@ -1,6 +1,13 @@
-class Task {
+import 'package:hive_flutter/adapters.dart';
+part 'data.g.dart';
+
+@HiveType(typeId: 0)
+class Task extends HiveObject {
+  @HiveField(0)
   String name = '';
+  @HiveField(1)
   bool isCompleted = false;
+  @HiveField(2)
   Priority priority = Priority.low;
 }
 
